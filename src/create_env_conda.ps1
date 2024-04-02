e21771258e35c67e5ca69e4eee6f4de3cc47902a
Maciej Kawęcki <skimiskim12@gmail.com>
2024-04-02 19:34:39 +0200

    Create conda env files
    Create script for creating env

---
+++ src/create_env_conda.ps1
@@ -0,0 +1,18 @@
+$envFile = "environments.yml"
+
+# Check if the environments.yml file exists
+if (-not (Test-Path $envFile)) {
+    Write-Host "Error: '$envFile' not found."
+    exit 1
+}
+
+# Get the environment name from the YAML file
+$envName = (Get-Content $envFile | Select-String -Pattern 'name:' | ForEach-Object { $_ -replace 'name:','' }).Trim()
+
+# Creating an environment from a YAML file
+conda env create -f $envFile
+
+# Activation of the environment
+conda activate $envName
+
+Write-Host "The environment '$envName' has been successfully created and activated."
